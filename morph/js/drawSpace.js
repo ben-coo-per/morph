e25bbd9f6a907c34c98d9615d31d40fe518c3117
Benjamin.a.cooper <cooperben83@gmail.com>
2020-06-20 08:56:48 -0500

    Vertex adjustments and other feature improvements

--- morph/js/drawSpace.js
+++ morph/js/drawSpace.js
@@ -3,17 +3,18 @@ var canvasSizes = {x: document.documentElement.clientWidth*0.4, y: document.docu
 var shapeArray = [];
 var tempArray = [];
 var renderPoly = {x:10,y:5};
-var renderBinary = 0; //0 = not rendering & 1 = rendering
+var renderBinary = 1; //0 = not rendering & 1 = rendering
 var vertexArray =[];
 
-// window.window.addEventListener("resize", resizeCanvas);
+window.addEventListener('resize', resizeCanvas);
 
+function resizeCanvas(){
 
-// resizeCanvas(){
-//   console.log('hi' + document.documentElement.clientWidth);
-//   canvasSizes = {x: document.documentElement.clientWidth*0.4, y: document.documentElement.clientHeight*0.6};
-//
-// }
+  var canvasSizes = {x: document.documentElement.clientWidth*0.4, y: document.documentElement.clientHeight*0.6};
+  myDrawSpace.resizeCanvas(canvasSizes.x,canvasSizes.y);
+  myThreeD.resizeCanvas(canvasSizes.x,canvasSizes.y);
+  myDrawSpace.clearDrawSpace()
+}
 
 var sketch1 = function(p){
 
@@ -23,40 +24,70 @@ var sketch1 = function(p){
   p.mirrorOn = 0; // 0 = no mirror & 1 = mirrors to other side
   p.hoverColor = {};
   p.vertexCleanBinary = 0;
+  p.drawMode = 'paint'; //vertex, paint, or erase
+  p.movingBinary = 0;
 
   // p.dxThresh = 1.50;
   // p.runBinary = 0; //0=shape path is not running on a curve & 1 = it is
   // p.runDirection; //0=running to the left & 1= to the right
-
-
-  p.numVertices = 7;
-
 
   p.setup = function() {
     //create scene
     p.canvas = p.createCanvas(canvasSizes.x,canvasSizes.y);
 
     p.drawSpace = p.createGraphics(p.canvas.width, p.canvas.height);
+    p.selectedCol = p.color(140); //selected button color
+    p.unselectedCol = p.color(230); //unselected button color
     // p.drawSpace.background(200);
   };
 
 //-----------standard draw loop---------
   p.draw = function(){
-    p.selectedCol = p.color(140); //selected button color
-    p.unselectedCol = p.color(230); //unselected button color
-    p.thicknessSlider = document.getElementById("thicknessSlider");
+    // p.thicknessSlider = document.getElementById("thicknessSlider");
+    document.getElementById("numVerticeSlider").max = p.shapeArray.length*0.65;
+    p.numVertices = document.getElementById("numVerticeSlider").value;
+    p.vertexCleaning();
+
 
     //draw mirror line
     p.drawSpace.line(p.canvas.width/2, 0, p.canvas.width/2, p.canvas.height);
 
+
+
+
+
     //display shapes
-    for (let i = 0; i < p.shapeArray.length; i++){
-      p.overBool = p.shapeArray[i].isOver();
-      if (p.overBool){
-        p.shapeArray[i].show(1);
-      } else{
-        p.shapeArray[i].show();}
-    };
+    if(p.drawMode == 'paint'){
+      for (let i = 0; i < p.shapeArray.length; i++){
+        p.overBool = p.shapeArray[i].isOver();
+        if (p.overBool){
+          p.shapeArray[i].showPaint(1);
+        } else{
+          p.shapeArray[i].showPaint();
+        }
+      }
+    } else if (p.drawMode == 'vertex') {
+      p.drawSpace.background(242);
+      p.drawSpace.stroke(1);
+      p.drawSpace.line(p.canvas.width/2, 0, p.canvas.width/2, p.canvas.height);
+      for (let i=0; i < p.vertShapeArray.length; i++){
+        if(i==0){
+          lastVertX = p.vertShapeArray[i].x;
+          lastVertY = p.vertShapeArray[i].y;
+        } else {
+          lastVertX = p.vertShapeArray[i-1].x;
+          lastVertY = p.vertShapeArray[i-1].y;
+        }
+
+        p.overBool = p.vertShapeArray[i].isOver();
+        if (p.overBool){
+          p.vertShapeArray[i].showVert(1, lastVertX, lastVertY);
+        } else{
+          p.vertShapeArray[i].showVert(0, lastVertX, lastVertY);
+        }
+      }
+
+    }
 
     p.image(p.drawSpace, 0, 0);
 
@@ -74,35 +105,55 @@ var sketch1 = function(p){
 
 
 p.vertexCleaning = function(){
-  console.log("before: " + p.shapeArray.length);
   p.cleanShapeArray();
   p.shapesToVertices();
-  console.log("after: " + p.shapeArray.length);
 }
 
-p.cleanShapeArray = function(numVertices){
+p.cleanShapeArray = function(){
   p.shapeArray = p.shapeArray.filter(function(value, index, arr){return value != undefined;});
 }
 
 //-------mouse dragged functionality
-  // p.mouseDragged = function(){          //this adds items to arrays and paints them on the drawSpace when mouse dragged
-  //   if (p.mouseX > 0 && p.mouseX < p.drawSpace.width && p.mouseY > 0 && p.mouseY < p.drawSpace.height){
-  //     p.thickness = p.thicknessSlider.value; //how thicc?
-  //     p.newShape = new p.shape();
-  //     p.shapeArray.push(p.newShape);
-  //
-  //     p.vertexCleanBinary = 1;
-  //
-  //     // if (p.mirrorOn == 1){
-  //     //   p.newMirroredShape = new p.shape(1); //variable is mirror bool
-  //     //   p.shapeArray.push(p.newMirroredShape);    //-> creation toggled in toggle.js
-  //     // }
-  //   }
-  // };
+  p.mouseDragged = function(){          //this adds items to arrays and paints them on the drawSpace when mouse dragged
+    if (p.drawMode == 'paint'){
+      if (p.mouseX > 0 && p.mouseX < p.drawSpace.width && p.mouseY > 0 && p.mouseY < p.drawSpace.height){
+        // p.thickness = p.thicknessSlider.value; //how thicc?
+        p.newShape = new p.shape();
+        p.shapeArray.push(p.newShape);
+        p.vertexCleanBinary = 1;
+        document.getElementById("numVerticeSlider").value = p.round(p.shapeArray.length*0.15);
+      }
+
+      if (p.mirrorOn == 1){
+        p.newMirroredShape = new p.shape(1); //variable is mirror bool
+        p.shapeArray.push(p.newMirroredShape);    //-> creation toggled in toggle.js
+      }
+
+
+    }
+    else if(p.drawMode == 'vertex'){
+      for (let i = 0; i < p.shapeArray.length; i++){
+        p.overBool = p.shapeArray[i].isOver();
+        if(p.overBool){
+          p.shapeArray[i].moveShape();
+          // console.log(p.movingBinary);
+        }
+      }
+      // p.movingBinary = 0;
+    }
+
+    // else if(p.drawMode == 'erase'){
+    //   //add erase functionality
+    // }
+  };
+
+  p.mouseReleased = function(){
+    p.movingBinary = 0;
+  }
 
   p.mousePressed = function(){
-    if (p.mouseX > 0 && p.mouseX < p.drawSpace.width && p.mouseY > 0 && p.mouseY < p.drawSpace.height){
-      p.thickness = p.thicknessSlider.value;
+    if (p.mouseX > 0 && p.mouseX < p.drawSpace.width && p.mouseY > 0 && p.mouseY < p.drawSpace.height && p.drawMode == 'paint'){
+      // p.thickness = p.thicknessSlider.value;
       p.newShape = new p.shape();
       p.shapeArray.push(p.newShape);
     }
@@ -120,44 +171,46 @@ p.cleanShapeArray = function(numVertices){
       this.x = p.mouseX;
     }
     this.y = p.mouseY;
-    this.thickness = p.thickness;
+    this.thickness = 16;
     this.brushChoice = p.brushChoice;
 
-    this.baseColor = p.color(40,30,100);
-    this.hoverColor = p.color(255, 204, 0);
+    this.baseColor = p.color(43,55,90);
+    this.hoverColor = p.color(220, 123, 90);
   };
 
   //show shape in draw mode
-    p.shape.prototype.showDMode = function(hoverBinary = 0){
+    p.shape.prototype.showPaint = function(hoverBinary = 0){
       //Set color based on T/F hover binary
       if (hoverBinary == 1){
         p.drawSpace.fill(this.hoverColor);
+
       } else {
         p.drawSpace.fill(this.baseColor);
       };
 
       p.drawSpace.noStroke();
-      //determine brushStroke --> this may be more useful later but if could translate to the vertices either being 'normal' vertices or curveVertices
-      if (this.brushChoice == 0){
         p.drawSpace.ellipse(this.x, this.y, this.thickness);
-      } else{
-        p.drawSpace.rectMode(p.CENTER);
-        p.drawSpace.rect(this.x, this.y, this.thickness, this.thickness);
-      }
+      //determine brushStroke --> this may be more useful later but if could translate to the vertices either being 'normal' vertices or curveVertices
+      // if (this.brushChoice == 0){
+      //   p.drawSpace.ellipse(this.x, this.y, this.thickness);
+      // } else{
+      //   p.drawSpace.rectMode(p.CENTER);
+      //   p.drawSpace.rect(this.x, this.y, this.thickness, this.thickness);
+      // }
     };
 
     //vertex mode show
-    p.shape.prototype.showVMode = function(hoverBinary = 0){
+    p.shape.prototype.showVert = function(hoverBinary = 0, lastVertX, lastVertY){
       //Set color based on T/F hover binary
       if (hoverBinary == 1){
         p.drawSpace.fill(this.hoverColor);
       } else {
         p.drawSpace.fill(this.baseColor);
       };
-
+      p.drawSpace.stroke(1);
+      p.drawSpace.line(lastVertX,lastVertY,this.x,this.y);
       p.drawSpace.noStroke();
       p.drawSpace.ellipse(this.x, this.y, this.thickness);
-      
     };
 
     //change hover binary if floating over
@@ -172,6 +225,12 @@ p.cleanShapeArray = function(numVertices){
         this.bottomBound = (this.y+this.thickness/2);
         return (this.leftBound < p.mouseX && p.mouseX < this.rightBound && this.topBound < p.mouseY && p.mouseY < this.bottomBound);
       }
+    };
+
+    p.shape.prototype.moveShape = function(){
+      p.movingBinary = 1;
+      this.x = p.mouseX;
+      this.y = p.mouseY;
     };
 
 
@@ -227,7 +286,15 @@ p.cleanShapeArray = function(numVertices){
   // }
 
 
-
+  p.shapesToVertices = function(){
+    p.vertShapeArray = [];
+    divideBy = p.round(p.shapeArray.length/p.numVertices);
+    for (let i= 0; i < p.shapeArray.length; i++){
+      if (i%(p.round(p.shapeArray.length/p.numVertices)) == 0){
+        p.vertShapeArray.push(p.shapeArray[i]);
+      }
+    }
+  }
 
 
 
@@ -236,6 +303,14 @@ p.cleanShapeArray = function(numVertices){
       let justCleared = p.shapeArray.splice(0, p.shapeArray.length);
       p.drawSpace.background(p.color(244, 241,242));
       p.drawSpace.stroke(1);
+    }
+    p.togglePaintBrush = function(){
+      //change colors of buttons
+      p.drawMode = 'paint';
+    }
+    p.toggleVertexBrush = function() {
+      //change colors of buttons
+      p.drawMode = 'vertex';
     }
     // p.toggleCircleBrush = function(){
     //   //change colors of buttons
@@ -248,7 +323,7 @@ p.cleanShapeArray = function(numVertices){
     // p.undoClear = function(){}
 
   p.export = function(){
-    shapeArray = p.shapeArray;
+    shapeArray = p.vertShapeArray;
   }
 
 };
@@ -260,6 +335,9 @@ var sketch2 = function(p){
 
   // p.sliceArray = [];
   p.rotationTracker = 0;
+  p.res = 30;
+  p.rotationAmount = 360;
+
   p.vertexArray = [];
 
   p.setup = function(array){
@@ -275,19 +353,19 @@ var sketch2 = function(p){
     p.gSlider = document.getElementById("gSlider");
     p.bSlider = document.getElementById("bSlider");
     // p.rotationAmount = document.getElementById('rotation slider');
-    p.rotationAmount = 360;
-    p.res = 30;
-    p.ambientLight(255,255,255);
+
+    // p.ambientLight(255,255,255);
+    // p.directionalLight(255,255,255);
     p.materialColor = p.color(p.rSlider.value, p.gSlider.value, p.bSlider.value)
+    p.noStroke();
+
+    // p.rotationAmount = document.getElementById("rotationSlider").value;
+    // p.res = document.getElementById("resolutionSlider").value;
 
 
-
-    // p.directionalLight(255,255,255,0,1,1,-1);
+    p.directionalLight(255,255,255,1,1,-1);
     // p.ambientLight(20);
     p.background(p.color(244, 241,242));
-
-
-    // p.ambientMaterial(p.materialColor);
 
 
     p.yAvg = p.findYCenter();
